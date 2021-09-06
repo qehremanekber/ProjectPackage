7ef73c20a261829c7a6bc9165e831c226464ccd3
Gahraman Akbarov <qehremanekber@gmail.com>
2021-09-06 21:48:39 +0200

    commit dana

---
+++ R/boxplot_var.R
@@ -0,0 +1,18 @@
+#' @param feat
+#' @param df
+#' @import plyr
+#' @import dplyr
+#' @import ggplot2
+#' @import tidyr
+#' @import GGally
+#' @export
+
+
+boxplot_var <- function(feat, df){
+  df %>%
+    ggplot(aes_string(x = as.factor('quality_bin'), y = feat, color = 'quality_bin')) +
+    geom_boxplot(outlier.color = "darkred", notch = FALSE) +
+    labs(title = paste0("Boxplot of feature: ", feat)) + ylab(feat) + xlab("Quality (1 = good, 2 = bad)") +
+    theme(legend.position = "none", axis.title.x = element_blank()) +
+    theme_minimal()
+}
